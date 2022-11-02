31f569b0dd27b90ac3bc443565eb49f4c90f0b46
Yogita Vaishnav <yogitavaishnav21@gmail.com>
2022-11-02 21:57:44 +0530

    added mulitply

---
+++ mul.java
@@ -0,0 +1,9 @@
+import javax.annotation.processing.SupportedOptions;
+
+public class mul {
+    public static void main(String[] args) {
+        int a=12;
+        int b=25;
+        System.out.println(a*b);
+    }
+}
